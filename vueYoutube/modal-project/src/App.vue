23fab30391800d04bc0e1e84556b7cf6572a0cad
Pranjal Kaura <pranjal17079@iiitd.ac.in>
2024-03-18 18:00:41 +0530

    Added modal project

---
+++ vueYoutube/modal-project/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <h1>{{title}}</h1>
+  <div v-if="showModal">
+    <Modal  theme="saleoff" @close="toggleModal">
+      <template v-slot:links>
+        <a href="">sign up now!</a>
+        <a href="">FAQ</a>
+      </template>
+      <h1>Heavy discount!</h1>
+      <p>Grab your Barca hoodies at 50% off!!</p>
+
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">open modal (alt)</button>
+
+  <!-- my modal -->
+  <teleport to=".modals" v-if="showModal2">
+    <Modal userName='Pranjal' @close2="toggleModal2">
+      <template v-slot:authorise>
+        <h1>Authorised access!!</h1>
+        <p>Hello Mr Kaura!!!</p>
+      </template>
+      <h1>Unauthorized access!!</h1>
+      <p>Unfortunately, this modal is only for selective users!</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal2">open second modal</button>
+
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {Modal},
+  data() {
+    return {
+      title: 'My first Vue App :)',
+      header: 'Sign up for the giveaway!',
+      text: "Buy Barca tshirt for 50% off",
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }, 
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueYoutube/modal-project/src/components/Modal.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme==='sale'}">
+          <slot></slot>
+          <div class="actions">
+            <slot name="links"></slot>
+          </div>
+        </div>
+    </div>
+    <br>
+    <div class="backdrop" @click.self="closeModal2">
+        <div class="modal" v-if="userName==='Pranjal'">
+          <slot name="authorise"></slot>
+        </div>
+        <div class="modal" v-else>
+          <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['theme', "userName"],
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    },
+    closeModal2() {
+      this.$emit('close2');
+    }
+  }
+}
+</script>
+
+
+<style>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 200px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.24);
+    width: 100%;
+    height: 100%;
+  }
+  .modal h1 {
+    color:green;
+    border: none;
+    padding: 0;
+  }
+  .modal p {
+    font-style: normal;
+    color: aqua;
+  }
+
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1{
+    color: white;
+  }
+
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
+    color: white;
+  }
+  </style>
